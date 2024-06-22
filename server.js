1be24e5fd95574616d28890716dab4ed286a48ea
Shreyas Shipate <131515904+shreyasshipate@users.noreply.github.com>
2024-06-22 21:18:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -246,6 +246,6 @@ app.get("/combined-data", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
